d395ac95ab1e3d6b63ebf2f18709eabb4428250c
vedantpawar18 <vedantpawar18@gmail.com>
2025-03-24 20:33:33 +0530

    server setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,37 @@
-const express = require('express');
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
 const app = express();
-const port = 3000;
- 
-app.use(express.json()); 
-app.get('/', (req, res) => {
-  res.send('Welcome to the MERN Furniture App!');
+
+const PORT = 8000;
+dotenv.config();
+connectDB();
+
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "Accept",
+    ],
+  })
+);
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "gharsofa server starts" });
 });
 
-  
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`server start running at ${PORT}`);
 });
