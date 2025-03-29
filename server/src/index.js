89b5134a3c177ca90602b574af94ff774f1d7ee9
vedantpawar18 <vedantpawar18@gmail.com>
2025-03-29 20:36:48 +0530

    db connection established

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import connectDB from "./config/connectDB.js";
 
 const app = express();
 
